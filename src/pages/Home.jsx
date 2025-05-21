383881b00d0c6624d3059d360b1b82ced7eff0bb
Janio <janiobachmann@gmail.com>
2025-05-21 15:59:24 +0200

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import profilePic from '../assets/foto_1.jpg';
+
+export const Home = () => {
+    return (
+        <div className="w-full">
+            <section className="w-full flex justify-center items-center">
+                <h1 className="text-4xl font-bold text-center">
+                    Janio
+                </h1>
+            </section>
+        </div>
+    );
+};
+
+export default Home;
